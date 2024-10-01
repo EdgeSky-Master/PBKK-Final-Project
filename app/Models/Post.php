b9b94e91728c91a79e97096ff008313c34890d31
FawwazAlJawi <faaljawi04@gmail.com>
2024-10-01 16:51:42 +0700

    After Migration and using Eloquent ORM

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,45 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post{
-    public static function all(){
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Fawwaz Abdulloh Al-Jawi',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus unde explicabo non fugiat in ipsam iure dicta laboriosam officia aut, aliquid eligendi. Officiis consectetur perspiciatis dicta, quisquam voluptas deserunt et.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Fawwaz Abdulloh Al-Jawi',
-                'body' => 'Possimus consequatur in fugit illum. Veritatis eveniet nihil magni asperiores nam soluta eaque unde iste, molestias quo ipsum quas maxime assumenda quibusdam?'
-            ],
-            [
-                'id' => 3,
-                'slug' => 'judul-artikel-3',
-                'title' => 'Judul Artikel 3',
-                'author' => 'Fawwaz Abdulloh Al-Jawi',
-                'body' => 'Ut nostrum non nihil aliquid dolorem et fugiat, eaque, suscipit ad velit cumque magnam qui, dignissimos vero alias quae earum repellendus ipsum.'
-            ]
-        ];
-    }
-
-    public static function find($slug): array{
-
-
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+class Post extends Model{
     
-        if(! $post){
-            abort(404);
-        }
-        return $post;
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
+
 
 ?>
