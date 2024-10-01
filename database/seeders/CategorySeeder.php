7b098567ab2a840a99f7fa0c48dd69e6f1bd1565
FawwazAlJawi <faaljawi04@gmail.com>
2024-10-02 06:45:32 +0700

    Added Database Seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name'=>'Food',
+            'slug'=>'food'
+        ]);
+        Category::create([
+            'name'=>'Beverages',
+            'slug'=>'beverages'
+        ]);
+        Category::create([
+            'name'=>'Snack',
+            'slug'=>'snack'
+        ]);
+    }
+}
