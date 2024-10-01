420acc1e97a26780005affaf7b2a85d6eccaa058
FawwazAlJawi <faaljawi04@gmail.com>
2024-10-01 22:38:11 +0700

    Added Factories, and Eloquent relationship up until Post Category

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
 use App\Models\Post;
-use Illuminate\Support\Arr;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Home Page'] );
+    return view('home', ['title' => 'Home'] );
 });
 
 Route::get('/about', function () {
-    return view('about', ['title' => 'about'], ['name' => 'Fawwaz Abdulloh Al-Jawi'] );
+    return view('about', ['title' => 'About'], ['name' => 'Fawwaz Abdulloh Al-Jawi'] );
 });
 
 Route::get('/posts', function () {
@@ -19,9 +20,17 @@ Route::get('/posts', function () {
 
 Route::get('/posts/{post:slug}', function (Post $post) {
 
-    // $post = Post::find($slug);
-
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user:username}', function (User $user) {
+
+    return view('posts', ['title' => count($user->posts) . ' Articles by '. $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+
+    return view('posts', ['title' => count($category->posts) . ' Articles on the topic '. $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
