a8e290f40cd8653f9b1ce2213c02b62d83c3042f
irul edgesky <irulannaba@gmail.com>
2024-10-16 13:45:39 +0700

    login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Controllers;
 
-class LoginController extends Controller{
-    public function index(){
-        return view('login',[
-            'title' => 'Login'
-        ]);
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Session;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        if (Auth::check()) {
+            return redirect('home');
+        } else {
+            return view('login');
+        }
     }
-    
+    public function loginAction(Request $request)
+    {
+        $data = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ];
+
+        if (Auth::Attempt($data)) {
+            return redirect('home');
+        } else {
+            Session::flash('error', 'Email atau Password Salah');
+            return redirect('/login');
+        }
+    }
+
+    public function logoutAction()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
 }
