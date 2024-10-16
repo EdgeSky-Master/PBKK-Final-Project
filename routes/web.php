ea8f4a15ad9a88f76367e9b75daf7ee7459e4b1b
irul edgesky <irulannaba@gmail.com>
2024-10-16 13:48:31 +0700

    try making login routing

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Home'] );
+    return view('home', ['title' => 'Home']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['title' => 'About'], ['name' => 'Fawwaz Abdulloh Al-Jawi'] );
+    return view('about', ['title' => 'About'], ['name' => 'Fawwaz Abdulloh Al-Jawi']);
 });
 
 Route::get('/posts', function () {
@@ -20,7 +20,7 @@ Route::get('/posts', function () {
     // if(request('search')){
     //     $posts->where('title', 'like', '%' . request('search') . '%');
     // }
-    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search','category','author']))->latest()->paginate(5)->withQueryString()]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(5)->withQueryString()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -30,12 +30,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/authors/{user:username}', function (User $user) {
     //$posts = $user->posts->load('category','author');
-    return view('posts', ['title' => count($user->posts) . ' Articles by '. $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
 
-    return view('posts', ['title' => count($category->posts) . ' Articles on the topic '. $category->name, 'posts' => $category->posts]);
+    return view('posts', ['title' => count($category->posts) . ' Articles on the topic ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
@@ -45,3 +45,10 @@ Route::get('/contact', function () {
 Route::get('/login', function () {
     return view('login', ['title' => 'Login']);
 });
+
+// Route::get('/login', [AuthController::class, 'login'])->name('login');
+// Route::post('loginAction', [AuthController::class, 'loginAction'])->name('loginAction');
+// Route::get('logoutAction', [AuthController::class, 'logoutAction'])->name('logoutAction')->middleware('auth');
+
+// Route::get('/registration', [RegisterController::class, 'registration'])->name('registration');
+// Route::post('registration', [RegisterController::class, 'registration'])->name('registration');
